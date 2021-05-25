08abc8d38c2afb3f72ea19184b1ac1f698edfc39
Ricardo Sifontes <jrsifontes16@gmail.com>
2021-05-24 21:07:15 -0600

    First commit

---
+++ src/COVID19-Reports.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace COVID19_Reports.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
