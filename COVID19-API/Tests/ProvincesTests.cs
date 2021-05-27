f46d6b61315e4274e909fae9f71698da5d124164
Ricardo Sifontes <jrsifontes16@gmail.com>
2021-05-27 12:56:46 -0600

    Finishing Test cases

---
+++ COVID19-API/Tests/ProvincesTests.cs
@@ -0,0 +1,40 @@
+﻿using COVID19_API.Controllers;
+using COVID19_API.DTOs.COVID19_API.Provinces;
+using COVID19_API.Responses.COVID19_API;
+using COVID19_API.Services.COVID19_API;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace COVID19_API.Tests
+{ 
+    [TestClass]
+    public class ProvincesTests
+    {
+
+    private ICOVID19_APIService _COVID19_APIService;
+
+    public ProvincesTests()
+    {
+        this._COVID19_APIService = new COVID19_APIService();
+    }
+
+        [TestMethod]
+        public async Task TestUSARegionsCountAsync()
+        {
+            var controller = new ProvincesController(_COVID19_APIService);
+            int regionsCount = 10;
+            var response = await controller.Get("USA");
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(regionsCount, response.Data.Count);
+        }
+
+        [TestMethod]
+        public async Task TestProvincesTypeAsync()
+        {
+            var controller = new ProvincesController(_COVID19_APIService);
+            var response = await controller.Get("USA");
+            Assert.IsInstanceOfType(response, typeof(BaseResponse<ProvincesTopTen>));
+        }
+    }
+}
---
+++ COVID19-API/Tests/RegionsTests.cs
@@ -0,0 +1,40 @@
+﻿using COVID19_API.Controllers;
+using COVID19_API.DTOs.COVID19_API.Regions;
+using COVID19_API.Responses.COVID19_API;
+using COVID19_API.Services.COVID19_API;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace COVID19_API.Tests
+{ 
+    [TestClass]
+    public class RegionsTests
+    {
+
+    private ICOVID19_APIService _COVID19_APIService;
+
+    public RegionsTests()
+    {
+        this._COVID19_APIService = new COVID19_APIService();
+    }
+
+        [TestMethod]
+        public async Task TestRegionsCountAsync()
+        {
+            var controller = new RegionsController(_COVID19_APIService);
+            int regionsCount = 214;
+            var response = await controller.Get();
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(regionsCount, response.Data.Count);
+        }
+
+        [TestMethod]
+        public async Task TestRegionsTypeAsync()
+        {
+            var controller = new RegionsController(_COVID19_APIService);
+            var response = await controller.Get();
+            Assert.IsInstanceOfType(response, typeof(BaseResponse<RegionDto>));
+        }
+    }
+}
