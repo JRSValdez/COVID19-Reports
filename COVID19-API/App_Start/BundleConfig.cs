e29adce46934d74add6506d16e533dc5e8c54f69
Ricardo Sifontes <jrsifontes16@gmail.com>
2021-05-26 23:01:43 -0600

    Adding First UI commit

---
+++ COVID19-API/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Web.Optimization;
+
+namespace COVID19_API
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customJS").Include(
+                      "~/Scripts/Select2/Select2.min.js",
+                      "~/Scripts/DataTables/datatables.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/customCSS").Include(
+                      "~/Content/Select2/Select2.min.css",
+                      "~/Content/DataTables/datatables.min.css"));
+        }
+    }
+}
